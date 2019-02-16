5b52c15204afa18ccd710833ae2c5b7cd3e3535c
eclipseglory <947734830@qq.com>
2019-02-17 01:16:57 +0800

    first commit

---
+++ test/test.js
@@ -0,0 +1,43 @@
+
+function createCompareCanvas(){
+    let margin = 10;
+    let body = document.body;
+    body.style.margin = margin;
+    body.style.padding = 0;
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    createCanvas('webgl');
+    createCanvas('canvas');
+    function createCanvas(id){
+        let webglCanvas = document.createElement('canvas');
+        webglCanvas.style.display = 'block';
+        webglCanvas.style.backgroundColor = 'black';
+        webglCanvas.id = id;
+        webglCanvas.width = w - margin*2;
+        webglCanvas.height = h/2 - margin*2;
+        webglCanvas.style.width = webglCanvas.width + "px";
+        webglCanvas.style.height = webglCanvas.height + "px";
+        document.body.append(webglCanvas);
+    }
+}
+
+function createOneFullCanvas(){
+    let margin = 10;
+    let body = document.body;
+    body.style.margin = margin;
+    body.style.padding = 0;
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    createCanvas('webgl');
+    function createCanvas(id){
+        let webglCanvas = document.createElement('canvas');
+        webglCanvas.style.display = 'block';
+        webglCanvas.style.backgroundColor = 'black';
+        webglCanvas.id = id;
+        webglCanvas.width = w - margin*2;
+        webglCanvas.height = h - margin*2;
+        webglCanvas.style.width = webglCanvas.width + "px";
+        webglCanvas.style.height = webglCanvas.height + "px";
+        document.body.append(webglCanvas);
+    }
+}
